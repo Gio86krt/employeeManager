53df00ab93e86b85ca5e1d27596a7b97e2af2b89
Giovanni <lugk4l@gmail.com>
2021-02-28 17:03:11 -0500

    update employee info implemented

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,7 @@
 const sql = require("mysql");
 const express = require("express");
-const inquirer = require("inquirer");
-const ormSearch = require("../assets/ormSearch");
-const ormUpdate = require("../assets/ormUpdate");
-const table = require("console.table");
+// const inquirer = require("inquirer");
+const starter = require("../assets/starter");
 const { CLIENT_RENEG_LIMIT } = require("tls");
 const { restoreDefaultPrompts } = require("inquirer");
 const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require("constants");
@@ -17,30 +15,7 @@ app.use(express.json());
 
 const db = require("./connection")("employees_db", "mySQL86kyokushin");
 
-async function getStarted() {
-  //welcomeMessage();
-  return (result = await inquirer
-    .prompt({
-      message: "What would you like to do today?",
-      type: "rawlist",
-      choices: ["SEARCH DATA", "UPDATE DATA"],
-      name: "action",
-    })
-    .then((res) => {
-      // console.log(res.action);
-      switch (res.action) {
-        case "SEARCH DATA":
-          ormSearch.searchData();
-          break;
-        case "UPDATE DATA":
-          ormUpdate.updateData();
-          break;
-      }
-    }));
-}
-
-getStarted();
-
 app.listen(port, () => {
   console.log(`Server si running at port: `, port);
+  starter();
 });
